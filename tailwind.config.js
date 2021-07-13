ab0cd33bf672b0d3452527f44c4d0bf9076a7631
Tanya <Leenders>
2021-07-13 16:44:01 +0200

    first version on mobile
    todo: improve tailwind classes, and obviously the responsive

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    theme: {
+        fontSize: {
+            '3xs': '.55rem',
+            '2xs': '.65rem',
+            'xs': '.75rem',
+            'sm': '.875rem',
+            'base': '1rem',
+            'lg': '1.125rem',
+            'xl': '1.25rem',
+            '2xl': '1.5rem',
+            '3xl': '1.875rem',
+            '4xl': '2.25rem',
+            '5xl': '3rem',
+            '6xl': '4rem',
+            '7xl': '5rem',
+        },
+        borderRadius: {
+            'none': '0',
+            'sm': '0.125rem',
+            DEFAULT: '0.25rem',
+            'md': '0.375rem',
+            'lg': '0.5rem',
+            'xl': '0.75rem',
+            '2xl': '1rem',
+            '3xl': '1.5rem',
+            '4xl': '2rem',
+            'full': '9999px',
+            'large': '12px',
+        }
+    }
+}
